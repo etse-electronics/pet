ff3c1ea2328c4388eeaf95558a073d2af2fa2e2c
Edwin Nel <nel.edwin@gmail.com>
2025-08-28 13:14:08 +0000

    cleanup

--- Program.cs
+++ Program.cs
@@ -12,28 +12,17 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UsePathBase("/api");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return "service is running";
+});
+
+app.MapGet("/ping", () =>
+{    
+    return "pong";
 })
-.WithName("GetWeatherForecast");
+.WithName("Ping");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
